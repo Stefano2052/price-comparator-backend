14d64850aba554daae3c1dce6d5a1938d35b49ea
Stefano Ventilii <stefano.ventilii@gmail.com>
2025-11-19 20:40:32 +0100

    TEMP: auto-create admin user

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# 👇 IMPORT PER IL COMANDO
+from django.core.management import call_command
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+
+# 👇 BLOCCO TEMPORANEO: crea superuser admin se non esiste
+try:
+    call_command("createsu")
+except Exception as e:
+    print(f"Auto createsu skipped/failed: {e}")
 
 application = get_wsgi_application()
