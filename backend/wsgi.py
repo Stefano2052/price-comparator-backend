57e58fe472fa054fe0ed06af7d24496a9840efe2
Stefano Ventilii <stefano.ventilii@gmail.com>
2025-11-22 22:33:41 +0100

    createsu

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,24 +1,6 @@
-"""
-WSGI config for backend project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
 from django.core.wsgi import get_wsgi_application
 
-# 👇 IMPORT PER IL COMANDO
-from django.core.management import call_command
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-
-# 👇 BLOCCO TEMPORANEO: crea superuser admin se non esiste
-try:
-    call_command("createsu")
-except Exception as e:
-    print(f"Auto createsu skipped/failed: {e}")
 
 application = get_wsgi_application()
